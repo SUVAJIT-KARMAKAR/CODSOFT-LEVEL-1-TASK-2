580aa92b9b10e05d3a93e3a27cd84186c029698b
SUVAJIT <suvajitk@192.168.1.3>
2024-07-19 23:13:33 +0530

    SUVAJIT KARMAKAR | easyFIND.prog | Page preloader set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
 // APP 
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import HashLoader from "react-spinners/HashLoader";
 import './App.scss';
 
 // COMPONENTS IMPOR 
 import Menu from './components/Menu';
+import Header from './components/Header';
 
 const App = () => {
+  const[loading, setloading] = useState(false);
+
+  useEffect( () => {
+      setloading(true)
+      setTimeout( () => {
+          setloading(false)
+      }, 5000);
+  }, []);
+
+
   return (
-    <>
-    <Menu/>
-    </>
+    <div className="app">
+      {
+        loading ? 
+        (
+          <div className="app-loader">
+            <HashLoader
+              className="app-loader__icon"
+              size={50}
+              color={"#15CDA8"}
+              loading={loading}
+            />   
+          </div>
+
+        )
+          : (
+            <>
+              <Menu/>
+              <Header/>             
+            </>
+          )
+
+      }
+    </div>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+// HEADER 
+
+const Header = () => {
+    return (
+        <div></div>
+    );
+};
+
+export default Header;
