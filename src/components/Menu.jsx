5c5da17cb6316c4fe7f69b46f4c6d8a53c356f78
SUVAJIT <suvajitk@192.168.1.3>
2024-07-19 21:39:47 +0530

    SUVAJIT KARMAKAR | easyFIND.prog | Menu component completed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,27 @@
 // MENU
+import { LuGitFork } from "react-icons/lu";
 
 const Menu = () => {
     return (
-        <div></div>
+        <div className="menu">
+            <div 
+                className="menu-title"
+            >
+                <h2> &lt;#/&gt; easyFIND. 
+                <span    
+                    className="menu-title__prog"   
+                >prog</span></h2>
+            </div>
+
+            <a href="#" target="_blank">
+                <div className="menu-fork">
+                    <LuGitFork  
+                        size={20}    
+                    />
+                </div>
+            </a>
+
+        </div>
     );  
 };
 
